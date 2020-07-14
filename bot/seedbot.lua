461e16616d3ed953d1929d8f45bed3b306c3c747
Dioxid-Code <67042447+Dioxid-Code@users.noreply.github.com>
2020-07-14 09:05:41 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {126070255},--Sudo users 
+    sudo_users = {1121667376},--Sudo users 
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
